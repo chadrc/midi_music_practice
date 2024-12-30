592629670368f5869f5ff518e8b7ea7da4877cca
Chad Collins <chadrcollins91@gmail.com>
2024-12-30 12:32:58 -0500

    Formatting notes

---
+++ src/notes/index.ts
@@ -0,0 +1,10 @@
+
+export const LETTER_NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+
+export const formatMidiNote = (midiNote: number) => {
+    // 0 is C-1
+    let letterNo = midiNote % 12;
+    let octave = Math.floor(midiNote / 12) - 1;
+
+    return `${LETTER_NOTES[letterNo]}${octave}`;
+}
--- src/store/midi.ts
+++ src/store/midi.ts
@@ -1,8 +1,26 @@
 import {defineStore} from 'pinia'
+import {formatMidiNote} from "../notes";
 
 interface IOState {
     receiving: boolean;
     sending: boolean;
+}
+
+enum MIDIInstruction {
+    NoteOn,
+    NoteOff,
+    PolyphonicAftertouch,
+    Aftertouch,
+    ControlChange,
+    ProgramChange,
+    PitchBend,
+}
+
+interface MIDIData {
+    instruction: MIDIInstruction;
+    channel: number;
+    data1: number;
+    data2: number;
 }
 
 interface MIDIStore {
@@ -53,7 +71,9 @@ export const useMidiStore = defineStore('midi', {
 
             if (currentState === undefined || currentState.receiving === false) {
                 device.onmidimessage = (event: MIDIMessageEvent) => {
-                    console.log(event.data);
+                    let data = makeMidiData(event.data);
+
+                    console.log(`${formatMidiNote(data.data1)} ${MIDIInstruction[data.instruction]}`);
                 }
 
                 this.ioStates.set(deviceId, {receiving: true, sending: false});
@@ -65,3 +85,44 @@ export const useMidiStore = defineStore('midi', {
         }
     }
 })
+
+function makeMidiData(bytes: Uint8Array): MIDIData | null {
+    let status = bytes[0];
+    let channel = 0;
+    let instruction: MIDIInstruction | null = null
+    let data1 = bytes[1];
+    let data2 = bytes[2];
+
+    if (status > 239) {
+        console.log("Unsupported MIDI instruction. Check back later for implementation.")
+        return null
+    } else if (status >= 224) {
+        instruction = MIDIInstruction.PitchBend;
+        channel = status - 224
+    } else if (status >= 208) {
+        instruction = MIDIInstruction.Aftertouch;
+        channel = status - 208
+    } else if (status >= 192) {
+        instruction = MIDIInstruction.ProgramChange;
+        channel = status - 192
+    } else if (status >= 176) {
+        instruction = MIDIInstruction.ControlChange;
+        channel = status - 176
+    } else if (status >= 160) {
+        instruction = MIDIInstruction.PolyphonicAftertouch;
+        channel = status - 160
+    } else if (status >= 144) {
+        instruction = MIDIInstruction.NoteOn;
+        channel = status - 144
+    } else if (status >= 128) {
+        instruction = MIDIInstruction.NoteOff;
+        channel = status - 128
+    }
+
+    return {
+        instruction: instruction,
+        channel: channel,
+        data1: data1,
+        data2: data2,
+    }
+}
