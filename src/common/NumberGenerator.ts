522535550c306db5ca104b9b38355b8e301a4631
Chad Collins <chadrcollins91@gmail.com>
2025-01-20 09:06:30 -0500

    Expanded NumberGenerator to specify bit length to get smaller numbers

--- src/common/NumberGenerator.ts
+++ src/common/NumberGenerator.ts
@@ -1,23 +1,27 @@
-const NUMBER_BIT_LENGTH = Math.ceil(Math.log2(Number.MAX_SAFE_INTEGER));
+const MAX_BIT_LENGTH = Math.ceil(Math.log2(Number.MAX_SAFE_INTEGER));
 
 export class NumberGenerator {
     private state: number;
-    private _seed: number;
+    readonly seed: number;
+    readonly bitLength: number;
+    readonly maxNumber: number;
 
-    constructor(seed: number = (Math.random() * Number.MAX_SAFE_INTEGER)) {
+    constructor(
+        seed: number = (Math.random() * Number.MAX_SAFE_INTEGER),
+        bitLength: number = 32,
+    ) {
+        this.bitLength = Math.max(1, Math.min(Math.round(bitLength), MAX_BIT_LENGTH));
+        this.maxNumber = Math.pow(2, this.bitLength);
+
         // ensure seed is whole number
         if (seed > -1 && seed < 1) {
             // given a fractional value, likely from Math.random()
-            seed = seed * Number.MAX_SAFE_INTEGER;
+            seed = seed * this.maxNumber;
         }
 
         // ensure whole number
-        this._seed = Math.round(seed);
+        this.seed = Math.round(seed);
         this.state = seed;
-    }
-
-    get seed(): number {
-        return this._seed;
     }
 
     reset() {
@@ -27,23 +31,42 @@ export class NumberGenerator {
     next(): number {
         let num = 0;
 
-        for (let i = 0; i < NUMBER_BIT_LENGTH; i++) {
+        for (let i = 0; i < this.bitLength; i++) {
             const output = this.state & 1;
             num = (num << 1) | output;
 
             const bit = (this.state ^ (this.state >> 1) ^ (this.state >> 2) ^ (this.state >> 3) ^ (this.state >> 5)) & 1;
-            this.state = (this.state >> 1) | (bit << NUMBER_BIT_LENGTH);
+            this.state = (this.state >> 1) | (bit << (this.bitLength - 1));
         }
 
-        return num / Number.MAX_SAFE_INTEGER;
+        return num / this.maxNumber;
     }
 
     range(min: number, max: number): number {
         const roll = this.next();
-        return (roll * (max - min)) + min;
+        const dif = max - min;
+        const base = roll * dif;
+        return base + min;
     }
 
     rangeI(min: number, max: number): number {
-        return Math.round(this.range(min, max));
+        const roll = this.range(min, max);
+        return Math.round(roll);
+    }
+
+    static bit8(seed: number = (Math.random() * Number.MAX_SAFE_INTEGER)): NumberGenerator {
+        return new NumberGenerator(seed, 8);
+    }
+
+    static bit16(seed: number = (Math.random() * Number.MAX_SAFE_INTEGER)): NumberGenerator {
+        return new NumberGenerator(seed, 16);
+    }
+
+    static bit32(seed: number = (Math.random() * Number.MAX_SAFE_INTEGER)): NumberGenerator {
+        return new NumberGenerator(seed, 32);
+    }
+
+    static bitMax(seed: number = (Math.random() * Number.MAX_SAFE_INTEGER)): NumberGenerator {
+        return new NumberGenerator(seed, MAX_BIT_LENGTH);
     }
 }
--- tests/common/NumberGenerator.spec.ts
+++ tests/common/NumberGenerator.spec.ts
@@ -7,10 +7,28 @@ test('Seed between 1 and 0 gets transformed to be non fractional', () => {
     expect(gen1.seed).to.be.greaterThan(1);
 });
 
+test('Bit length clamped to 1', () => {
+    const gen1 = new NumberGenerator(Math.random(), 0);
+
+    expect(gen1.bitLength).to.be.equal(1);
+});
+
+test('Bit length clamped to bit length of Number.MAX_SAFE_INTEGER', () => {
+    const gen1 = new NumberGenerator(Math.random(), 1000);
+
+    expect(gen1.bitLength).to.be.equal(Math.ceil(Math.log2(Number.MAX_SAFE_INTEGER)));
+});
+
 test('Non-whole number seed get rounded', () => {
     const gen1 = new NumberGenerator(15.5);
 
     expect(gen1.seed).to.be.equal(16);
+});
+
+test('Max number calculated correctly', () => {
+    const gen1 = new NumberGenerator(15.5, 4);
+
+    expect(gen1.maxNumber).to.be.equal(16);
 });
 
 test('Two generators generate different values', () => {
@@ -37,17 +55,41 @@ test('One generator that is reset generates same value', () => {
 test('Range returns number between given numbers', () => {
     const gen1 = new NumberGenerator(100);
     const val1 = gen1.range(10.5, 25.12234);
+    const val2 = gen1.range(10.5, 25.12234);
 
     expect(val1).to.be.lessThanOrEqual(25.12234);
     expect(val1).to.be.greaterThanOrEqual(10.5);
+    expect(val1).to.not.be.equal(val2);
 });
 
 test('RangeI returns integer between given numbers', () => {
     const gen1 = new NumberGenerator(100);
     const val1 = gen1.rangeI(10, 25);
+    const val2 = gen1.rangeI(10, 25);
     const parts = val1.toString().split(".");
 
     expect(val1).to.be.lessThanOrEqual(25);
     expect(val1).to.be.greaterThanOrEqual(10);
     expect(parts[1]).toBeUndefined();
+    expect(val1).to.not.be.equal(val2);
+});
+
+test('bit8 sets bit length to 8', () => {
+    const gen = NumberGenerator.bit8();
+    expect(gen.bitLength).to.be.equal(8);
+});
+
+test('bit16 sets bit length to 16', () => {
+    const gen = NumberGenerator.bit16();
+    expect(gen.bitLength).to.be.equal(16);
+});
+
+test('bit32 sets bit length to 32', () => {
+    const gen = NumberGenerator.bit32();
+    expect(gen.bitLength).to.be.equal(32);
+});
+
+test('bitMax sets bit length to bit length of Number.MAX_SAFE_INTEGER', () => {
+    const gen = NumberGenerator.bitMax();
+    expect(gen.bitLength).to.be.equal(Math.ceil(Math.log2(Number.MAX_SAFE_INTEGER)));
 });
