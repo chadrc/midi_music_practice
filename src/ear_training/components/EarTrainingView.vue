4359adff30d5754c3a349db260c597afc1433190
Chad Collins <chadrcollins91@gmail.com>
2025-02-23 19:26:11 -0500

    Implemented answer submission

--- src/ear_training/components/EarTrainingView.vue
+++ src/ear_training/components/EarTrainingView.vue
@@ -1,38 +1,69 @@
 <script setup lang="ts">
 import {Button} from "primevue";
 import {useInstrumentStore} from "../../store/instruments";
-import {BaseNotes, CHROMATIC_SCALE_SET_NAME, SCALES} from "../../notes/scales";
-import {computed, ref} from "vue";
+import {ref} from "vue";
 import {NumberGenerator} from "../../common/NumberGenerator";
-import NoteGrid from "../../components/NoteGrid.vue";
 import NumberRange from "../../common/NumberRange";
+import {formatMidiNote, LETTER_NOTES} from "../../notes";
 
 const instrumentStore = useInstrumentStore()
 const generator = new NumberGenerator()
 const minNote = 24
 const maxNote = 108
-const scale = SCALES[CHROMATIC_SCALE_SET_NAME][BaseNotes.C]
+const numberOfOctaves = Math.floor((maxNote - minNote) / 12)
 
 const currentNote = ref(0)
-
-const currentOctave = computed(() => Math.floor(currentNote.value / 12) - 2)
-const octaveNotes = computed(() => {
-  let start = currentOctave.value * 12
-  let range = new NumberRange(start, start + 12)
-  let notes = []
-  for (let n of range) {
-    notes.push(n);
-  }
-
-  return notes;
-})
+const selectedNote = ref(null)
+const selectedOctave = ref(null)
+const answered = ref(false)
+const noteCorrect = ref(null)
+const octaveCorrect = ref(null)
 
 function setNote() {
   currentNote.value = generator.rangeI(minNote, maxNote)
 }
 
 function playNote() {
-  instrumentStore.playNote(BaseNotes.C + 12 * 4, 100, 500);
+  instrumentStore.playNote(currentNote.value, 100, 500);
+}
+
+function selectNote(note: number) {
+  selectedNote.value = note;
+}
+
+function selectOctave(octave: number) {
+  selectedOctave.value = octave;
+}
+
+function submit() {
+  let octave = Math.floor((currentNote.value) / 12) - 1
+  let note = (currentNote.value) % 12
+
+  let octaveAnswer = (selectedOctave.value + 1)
+
+  if (selectedNote.value === note) {
+    noteCorrect.value = true
+  }
+
+  if (octaveAnswer === octave) {
+    octaveCorrect.value = true
+  }
+
+  answered.value = true
+
+  console.log("Octave Answer: ", octave);
+  console.log("Octave Guess: ", octaveAnswer)
+  console.log("Note Guess: ", note)
+  console.log("Note Answer: ", currentNote.value)
+}
+
+function next() {
+  setNote()
+  answered.value = false
+  noteCorrect.value = null
+  octaveCorrect.value = null
+  selectedNote.value = null
+  selectedOctave.value = null
 }
 
 setNote()
@@ -40,23 +71,123 @@ setNote()
 </script>
 
 <template>
-  <h1>Ear Training</h1>
-  <Button
-    label="Play"
-    severity="info"
-    size="small"
-    @click="playNote"
-  />
-  <NoteGrid
-    :notes="octaveNotes"
-    :scale="scale"
-    :headers="[]"
-    :columns="12"
-    note-format="letter"
-    :hints="[]"
-  />
+  <section>
+    <div
+      class="prompt-card"
+      @click="playNote"
+    >
+      <span
+        class="prompt-text"
+      >
+        <span>{{ answered ? formatMidiNote(currentNote) : "?" }}</span>
+      </span>
+    </div>
+  </section>
+  <section class="options">
+    <div class="option-row">
+      <div
+        v-for="n in new NumberRange(0, numberOfOctaves)"
+        :key="n"
+        class="option-cell"
+        :class="{selected: selectedOctave === n}"
+        @click="selectOctave(n)"
+      >
+        {{ n + 1 }}
+      </div>
+    </div>
+    <div class="option-row">
+      <div
+        v-for="n in new NumberRange(0, 12)"
+        :key="n"
+        class="option-cell"
+        :class="{selected: selectedNote === n}"
+        @click="selectNote(n)"
+      >
+        {{ LETTER_NOTES[n] }}
+      </div>
+    </div>
+  </section>
+  <section>
+    <Button
+      v-if="answered"
+      label="Next"
+      @click="next"
+    />
+    <Button
+      v-else
+      label="Submit"
+      @click="submit"
+    />
+  </section>
+  <section
+    v-if="answered"
+    class="answers"
+  >
+    <div>
+      Octave {{ octaveCorrect ? "Correct" : "Wrong" }}
+    </div>
+    <div>
+      Note {{ noteCorrect ? "Correct" : "Wrong" }}
+    </div>
+  </section>
 </template>
 
 <style scoped>
 
+section {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 1rem;
+}
+
+.options, .answers {
+  flex-direction: column;
+}
+
+.option-row {
+  display: flex;
+  flex-direction: row;
+  margin-top: 1rem;
+}
+
+.option-cell {
+  cursor: pointer;
+  display: flex;
+  width: var(--note-test-grid-cell-size);
+  height: var(--note-test-grid-cell-size);
+  justify-content: center;
+  align-items: center;
+  background-color: var(--p-neutral-800);;
+  transition: opacity 0.2s;
+}
+
+.option-cell.selected {
+  background-color: var(--p-neutral-600);
+}
+
+.option-cell:hover {
+  background-color: var(--p-neutral-400);
+}
+
+.prompt-card {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+  aspect-ratio: 1;
+  border-radius: 50%;
+  font-size: 2rem;
+  background-color: var(--p-neutral-800);
+  cursor: pointer;
+}
+
+.prompt-text {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: bold;
+}
 </style>
